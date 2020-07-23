c7548cc2868e0398582c188920a01e6972126e30
Maykboy <maykboy@gmail.com>
2020-07-23 17:16:10 +0300

    create: media reviews

--- js/main.js
+++ js/main.js
@@ -23,8 +23,4 @@ var reviewsSlider = new Swiper('.reviews-slider', {
     nextEl: '.reviews-slider__button--next',
     prevEl: '.reviews-slider__button--prev',
   },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
 });
