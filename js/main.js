97b1421fea7c548ca3fc503ff7707f5fc8ceae33
Maykboy <maykboy@gmail.com>
2020-07-24 10:02:36 +0300

    create: parallax

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   effect: "coverflow",
   keyboard: {
@@ -14,13 +14,19 @@ var hotelSlider = new Swiper('.hotel-slider', {
   },
 });
 
-var reviewsSlider = new Swiper('.reviews-slider', {
+var reviewsSlider = new Swiper(".reviews-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
   },
 });
+
+$(".newsletter").parallax({ imageSrc: "../img/newsletter-bg.jpeg" });
